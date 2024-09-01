bd966909d2299afca1a402b8121caefdd1a9e5ec
Cingciong <kulakesikula@gmail.com>
2024-09-01 20:55:50 +0200

    first commit

---
+++ resources/js/components/app.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>App Component</h1>
+    </div>
+    <main>
+        <router-view></router-view>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
